132e35d22f315ffa0264f7bf3fec0f35dd6d171b
strenuously-strenuous <ayaan.arena@gmail.com>
2023-12-26 10:49:37 +0530

    added all btws blogs

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// var cursor = new MouseFollower();
+// Get all elements with the class .insta-trigger
+
+// const blogswiper = new Swiper("#blog-h-slider-wrapper", {
+//   direction: "horizontal",
+//   loop: true,
+//   slidesPerView: 1,
+//   autoplay: {
+//     delay: 1500,
+//     disableOnInteraction: false,
+//   },
+// });
+
